616e6a5d814c84e2c079ce9ceda2bb6f34ea0a2c
Malo BOUCE <mabouce@e1r10p17.42.fr>
2019-01-23 13:28:16 +0100

    removing free in ft_error

--- ft_error_miniprintf.c
+++ ft_error_miniprintf.c
@@ -6,7 +6,7 @@
 /*   By: mabouce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 15:47:22 by mabouce           #+#    #+#             */
-/*   Updated: 2019/01/23 12:41:57 by mabouce          ###   ########.fr       */
+/*   Updated: 2019/01/23 13:27:31 by mabouce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_error_miniprintf(t_stru *stru)
 {
-	ft_strdel(&(stru->format));
-	ft_strdel(&(stru->tmp));
-	ft_strdel(&(stru->output));
 	ft_putstr("\nAn error occured.\n");
 	exit(0);
 }
