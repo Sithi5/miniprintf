3fcf504b41f037588031a74a71c4d71ae41363e0
Malo BOUCE <mabouce@e1r10p17.42.fr>
2019-01-23 13:29:29 +0100

    minor fix

--- ft_error_miniprintf.c
+++ ft_error_miniprintf.c
@@ -6,13 +6,13 @@
 /*   By: mabouce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 15:47:22 by mabouce           #+#    #+#             */
-/*   Updated: 2019/01/23 13:27:31 by mabouce          ###   ########.fr       */
+/*   Updated: 2019/01/23 13:29:14 by mabouce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniprintf.h"
 
-void	ft_error_miniprintf(t_stru *stru)
+void	ft_error_miniprintf(void)
 {
 	ft_putstr("\nAn error occured.\n");
 	exit(0);
